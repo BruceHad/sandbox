6fe3065c56004c805342622f37c5beba5fa330b7
treerock <treerock@myopera.com>
2013-11-19 13:43:47 +0000

    Add in Sliding boxes

--- project.php
+++ project.php
@@ -17,13 +17,32 @@ $sb_array = array(
         "url"     => "//sandbox.treerock.me/convert/",
         "date"    => "2013-11-01",
         "desc"    => "<p>'Metric Convertions' is an experiment with <a href='http://www.angularjs.org/'>AngularJS</a>. It converts various values (.e.g Temperatures and Heights) from metric to imperial and other scales. I've tried to make heavy use of Angular, e.g. the html (view) is built from data stored in Javascript objects (model) so the page really doesn't work unless Javascript is enabled.</p>
-                      <p>It also used <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Using_CSS_transitions'>CSS Transitions</a> to make simple animated elements on the page.</p>"
+                      <p>It also used <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Using_CSS_transitions'>CSS Transitions</a> to make simple animated elements on the page.</p>",
+        "status"  => "IP" // In progress
     ),
+    array (
+        "name"    => "Sliding Boxes",
+        "url"     => "//sandbox.treerock.me/sliding-boxes/",
+        "date"    => "2013-10-01",
+        "desc"    => "<p>Playing with AngularJS and CSS transitions.</p>",
+        "status"  => "C" // Complete
+    ),
+    array (
+        "name"    => "Image Loading",
+        "url"     => "//sandbox.treerock.me/sb-image-loading/",
+        "date"    => "2013-06-01",
+        "desc"    => "<p>HTML5 included a <a href='http://www.w3.org/TR/FileAPI/'>File API</a> that provides access to local files.
+                      This makes it quite easy to quickly drag and drop images into a web application and manipulate them without having to upload them to the client.</p>
+                      <p>This version is a quick go at drag and drop. I'm quite keen to try manipulating the image onscreen later.</p>",
+        "status"  => "IP", // In progress
+        "version" => 0.1
+    ),    
     array (
         "name"    => "Name",
         "url"     => "//sandbox.treerock.me/name/",
         "date"    => "2099-11-01",
-        "desc"    => "<p>Blank Description of the project"
+        "desc"    => "<p>Blank Description of the project.</p>",
+        "status"  => "NR" // Not ready
     )
 );
 
