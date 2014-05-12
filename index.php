579be4fd9fd4a50996949d0365cccb6d71de103c
treerock <treercock@myopera.com>
2014-05-12 21:47:16 +0100

    Tidy up

--- index.php
+++ index.php
@@ -27,10 +27,6 @@
                 <a class="navbar-brand" href="http://www.treerock.me/">Home</a>
             </div>
             <ul class="nav navbar-nav">
-                <li><a href="http://www.treerock.me/blog/">Blog</a>
-                </li>
-                <li><a href="http://www.treerock.me/work/">Work</a>
-                </li>
                 <li class="active"><a href="http://sandbox.treerock.me/">Sandbox</a>
                 </li>
             </ul>
@@ -52,27 +48,28 @@
     $sb_array = array(
         array (
             "name"    => "World Cup Sweep",
-            "url"     => "//sandbox.treerock.me/sweep/",
+            "url"     => "//sandbox.treerock.me/projects/sweep/",
             "date"    => "2014-04-01",
-            "desc"    => "<p>Another <a href='http://www.angularjs.org/'>AngularJS</a>. This one a CRUD application for creating Sweep Compeitions, where a group of friend can pick teams (randomly) until there is a winner.</p>",
+            "desc"    => "<p>Another <a href='http://www.angularjs.org/'>AngularJS</a>. This one a CRUD application for creating sweepstake style competitions, where a group of friend can pick teams (randomly) from some sporting event or something.</p>",
+            "status"  => "C",
+            "version" => 1.0,
+            "repo"    => "//github.com/treerock/sweep"
+        ),
+        array (
+            "name"    => "Sliding Boxes",
+            "url"     => "//sandbox.treerock.me/projects/sliding-boxes/",
+            "date"    => "2013-10-01",
+            "desc"    => "<p>Playing with CSS transitions.</p>",
             "status"  => "C",
             "version" => 1.0
         ),
         array (
-            "name"    => "Sliding Boxes",
-            "url"     => "//sandbox.treerock.me/sliding-boxes/",
-            "date"    => "2013-10-01",
-            "desc"    => "<p>Playing with CSS transitions.</p>",
-            "status"  => "C" // Complete
-            "version" => 1.0
-        ),
-        array (
             "name"    => "Image Loading",
-            "url"     => "//sandbox.treerock.me/sb-image-loading/",
+            "url"     => "//sandbox.treerock.me/projects/sb-image-loading/",
             "date"    => "2013-06-01",
             "desc"    => "<p>HTML5 included a <a href='http://www.w3.org/TR/FileAPI/'>File API</a> that provides access to local files.
                           This makes it quite easy to quickly drag and drop images into a web application and manipulate them without having to upload them to the client.</p>
-                          <p>This version is a quick go at drag and drop. I'm quite keen to try manipulating the image onscreen later.</p>",
+                          <p>This version is a quick go at drag and drop.</p>",
             "status"  => "IP", // In progress
             "version" => 0.1
         )
@@ -87,6 +84,8 @@
             $template .= "<h1>".$sb_array[$i]["name"]."</h1>";
             $template .= "<time datetime='".$sb_array[$i]["date"]."'>".$date_month."</time>";
             $template .= "<div class='sb-description'>".$sb_array[$i]["desc"]."</div>";
+            $template .= "<p><a href='".$sb_array[$i]["url"]."'>Link</a> | <a href='".$sb_array[$i]["repo"]."'>Repo</a></p>";
+            $template .= "</div>";
             echo($template);
         }
     } 
