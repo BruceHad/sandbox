a8c9ea31a961bbbb688e5a52f9fe0561baf0c5b7
treerock <treerock@myopera.com>
2014-05-12 20:23:42 +0000

    Tidy up

--- index.php
+++ index.php
@@ -2,18 +2,16 @@
 <html>
 
 <head>
-    <title>Treerock Sandbox Projects</title>
+    <title>Treerock Sandbox</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="http://treerock.me/assets/favicon.ico" />
-    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Vollkorn:400italic,400" />
-
+   
     <!-- Stylesheets -->
     <link href="css/normalize.css" rel="stylesheet" />
     <link href="css/base.css" rel="stylesheet" media="screen" />
     <link href="css/sandbox.css" rel="stylesheet" media="screen" />
-    <link href="css/project.css" rel="stylesheet" media="screen" />
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -40,17 +38,67 @@
     </header>
 
     <div class="container main clearfix">
-        <?php include('project.php'); ?>
-    </div>
+        <div class="row heading clearfix">
+            <h1>Treerock</h1>
+            <p class="subheading">
+                Sandbox Projects
+                <br />Learning
+                <br />\ experiments
+            </p>
+        </div>
+        <div class="sandbox">
+<?php
+
+    $sb_array = array(
+        array (
+            "name"    => "World Cup Sweep",
+            "url"     => "//sandbox.treerock.me/sweep/",
+            "date"    => "2014-04-01",
+            "desc"    => "<p>Another <a href='http://www.angularjs.org/'>AngularJS</a>. This one a CRUD application for creating Sweep Compeitions, where a group of friend can pick teams (randomly) until there is a winner.</p>",
+            "status"  => "C",
+            "version" => 1.0
+        ),
+        array (
+            "name"    => "Sliding Boxes",
+            "url"     => "//sandbox.treerock.me/sliding-boxes/",
+            "date"    => "2013-10-01",
+            "desc"    => "<p>Playing with CSS transitions.</p>",
+            "status"  => "C" // Complete
+            "version" => 1.0
+        ),
+        array (
+            "name"    => "Image Loading",
+            "url"     => "//sandbox.treerock.me/sb-image-loading/",
+            "date"    => "2013-06-01",
+            "desc"    => "<p>HTML5 included a <a href='http://www.w3.org/TR/FileAPI/'>File API</a> that provides access to local files.
+                          This makes it quite easy to quickly drag and drop images into a web application and manipulate them without having to upload them to the client.</p>
+                          <p>This version is a quick go at drag and drop. I'm quite keen to try manipulating the image onscreen later.</p>",
+            "status"  => "IP", // In progress
+            "version" => 0.1
+        )
+    );
+
+
+    for ($i=0; $i < count($sb_array); $i++){
+        if($sb_array[$i]["status"] == "C"){
+            $date_month = date('M-Y',strtotime($sb_array[$i]["date"]));
+            $template = "";
+            $template .= "<div class='item'>";
+            $template .= "<h1>".$sb_array[$i]["name"]."</h1>";
+            $template .= "<time datetime='".$sb_array[$i]["date"]."'>".$date_month."</time>";
+            $template .= "<div class='sb-description'>".$sb_array[$i]["desc"]."</div>";
+            echo($template);
+        }
+    } 
+    ?>
+        </div>
+    </div><!-- .container -->
 
     <footer>
         <div class="container">
             <p>Fais ce que tu veux.</p>
         </div>
     </footer>
-    <!--<script src="//code.jquery.com/jquery.js"></script>-->
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-    <script src="scripts/project.js"></script>
 </body>
 
 </html>
