6ee84d6ace69a55347ef757974ed7bc1e1786b3a
treerock <treerock@myopera.com>
2014-07-21 15:19:31 +0000

    Add EmmaD

--- index.php
+++ index.php
@@ -40,6 +40,17 @@
 <?php
 
     $sb_array = array(
+       array (
+        	"type"	  => "Web App",
+            "tags"	  => "js, angular",
+            "name"    => "Do Something - To do List",
+            "url"     => "//sandbox.treerock.me/projects/do-something/app/",
+            "date"    => "2014-06-09",
+            "desc"    => "<p>A practice project for learning the <a href='http://www.angularjs.org/'>AngularJS</a> framework. The aim was to build a basic, but functional <acronym title='Cread Read Update Delete'>CRUD</acronym> application.</p><p>It's not a todo app! Okay, it's kinda like a todo app. The idea is to provide a way to record daily tasks that you want to complete every day, and give you the satisfaction of ticking them off. So yeah, it's yet another To Do app, but for regular, recurring tasks.</p>",
+            "status"  => "C",
+            "version" => 1.0,
+            "repo"    => "//github.com/treerock/do-something"
+        ),
         array (
         	"type"	  => "Web App",
             "tags"	  => "js, angular",
@@ -68,8 +79,18 @@
             "url"     => "//sandbox.treerock.me/projects/imageload/",
             "date"    => "2013-06-01",
             "desc"    => "<p>HTML5 included a <a href='http://www.w3.org/TR/FileAPI/'>File API</a> that provides access to local files. This makes it quite easy to quickly drag and drop images into a web application and manipulate them without having to upload them to the client.</p><p>This version is a quick go at drag and drop.</p>",
-            "status"  => "C", // In progress
+            "status"  => "C",
             "version" => 0.1
+        ),
+		array (
+            "type"	  => "Web",
+            "tags"	  => "development, commercial, wordpress",
+            "name"    => "Emma Duff - Furniture, Restoration and French Polishing",
+            "url"     => "http://emmadfrenchpolishing.co.uk/",
+            "date"    => "2011-01-31",
+            "desc"    => "<p>[Last Updated July 2014] Website for furniture, restoration business based in the north of Scotland.</p>",
+            "status"  => "C",
+            "version" => 3
         )
     );
 	for ($i=0; $i < count($sb_array); $i++){
